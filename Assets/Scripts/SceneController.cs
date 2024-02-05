4ef73f5df2b05593255b2feb08b3019b1a0ebdc9
CiemnyWilk <117241918+CiemnyWilk@users.noreply.github.com>
2024-02-04 18:42:08 -0700

    Change enemy color randomly

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -18,7 +18,7 @@ public class SceneController : MonoBehaviour
             rend = enemy.GetComponent<Renderer>();
             enemy.transform.position = new Vector3(0, 1, 0);
             enemy.transform.localScale = new Vector3(1f, Random.Range(0.5f, 3f), 1);
-            rend.material.color = Color.red;
+            rend.material.color = Random.ColorHSV(0f,1f,1f,1f,0.5f,1f);
             float angle = Random.Range(0, 360);
             enemy.transform.Rotate(0, angle, 0);
         }
